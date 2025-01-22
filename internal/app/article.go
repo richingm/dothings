dabe3e175fbde892263c1713dd2c4b12f8ac7fed
mabaicun <1060220963@qq.com>
2025-01-22 13:26:28 +0800

    feat:ad

--- internal/app/article.go
+++ internal/app/article.go
@@ -1,5 +1,10 @@
 package app
 
+import (
+	"context"
+	"richingm/dothings/internal/entity"
+)
+
 type ArticleApp struct {
 }
 
@@ -7,6 +12,27 @@ func NewArticleApp() *ArticleApp {
 	return &ArticleApp{}
 }
 
-func (*ArticleApp) GetArticlesByBookId() {
+// GetArticlesByBookId 根据书籍id获取所有文章
+func (*ArticleApp) GetArticlesByBookId(ctx context.Context, bookId int64) ([]entity.ArticleDo, error) {
+	return nil, nil
+}
 
+// GetArticle 获取文章信息
+func (*ArticleApp) GetArticle(ctx context.Context, id int64) (*entity.ArticleDo, error) {
+	return nil, nil
+}
+
+// AddArticle 添加文章
+func (*ArticleApp) AddArticle(ctx context.Context, do entity.ArticleDo) (*entity.ArticleDo, error) {
+	return nil, nil
+}
+
+// UpdateArticle 修改文章
+func (*ArticleApp) UpdateArticle(ctx context.Context, id int64, do entity.ArticleDo) (*entity.ArticleDo, error) {
+	return nil, nil
+}
+
+// DelArticle 删除文章
+func (*ArticleApp) DelArticle(ctx context.Context, id int64) error {
+	return nil
 }
